94e7409f0b447144128c8d67040fb1d683f96ea8
Prasant <coderprasant@gmail.com>
2025-03-15 20:59:00 +0545

    Create email_validator.php

---
+++ email_validator.php
@@ -0,0 +1,8 @@
+<?php
+echo "=========================================\n";
+echo "|                                       |\n";
+echo "|  IF YOU WANT TO BUY THE WHOLE SCRIPT  |\n";
+echo "|  CONTACT ME ON TELEGRAM @WITCHSHOPHUB |\n";
+echo "|                                       |\n";
+echo "=========================================\n";
+?>
